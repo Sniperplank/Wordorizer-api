e6d4644908663b46095ea5d1a0ed767715601d1f
Eilaf Aljundi <elafjundi@gmail.com>
2023-03-02 00:08:19 -0500

    added words to my account

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import userRoutes from './routes/users.js'
 import wordRoutes from './routes/words.js'
+import fs from 'fs'
+import readLine from 'readline'
+import Word from './models/word.js'
+
 
 const app = express()
 dotenv.config()
@@ -24,3 +28,23 @@ const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.DB_CONNECTION)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
+
+
+// async function importWords() {
+//     const fileStream = fs.createReadStream('words.txt');
+//     const rl = readLine.createInterface({
+//         input: fileStream,
+//         crlfDelay: Infinity
+//     });
+
+//     rl.on('line', async (line) => {
+//         const [word, definition] = line.split(': ');
+//         await Word.create({ word, definition, userId });
+//     })
+
+//     rl.on('close', () => {
+//         console.log('Import completed');
+//     });
+// }
+
+// importWords()
